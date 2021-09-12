cad442cfcb7e1e03afeea10d409a0c88acdc6f26
Maggie Dulcie <mdulcie@et-k4.kzn.21-school.ru>
2021-09-12 19:03:56 +0300

    почти готово

---
+++ inseption/SRCS/requirements/wordpress/tools/wp_conf_start.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+cd /var/www/html/wordpress
+wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER_ADMIN --dbpass=$DB_PASSWORD_ADMIN --dbhost=$DB_HOST --dbprefix=mdulcie
+wp core install --allow-root --url=$DB_HOST --title=mdulcie.ru --admin_user=$DB_USER_ADMIN  --admin_password=$DB_PASSWORD_ADMIN --admin_email=$ADMIN_MAIL
+wp user create --allow-root $USER_NAME $USER_MAIL --role=author --user_pass=$USER_PASS
+php-fpm7.3 -F
