8fd96f50946780d0904959af5783130ce1824189
Maggie Dulcie <mdulcie@ev-h4.kzn.21-school.ru>
2021-10-02 21:39:56 +0300

    redis

--- inception/srcs/requirements/wordpress/tools/wp_conf_start.sh
+++ inception/srcs/requirements/wordpress/tools/wp_conf_start.sh
@@ -2,9 +2,11 @@
 
 cd /var/www/html/wordpress
 sleep 15
+if [ ! -e /var/www/html/wordpress/wp-config.php ]; then
 wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER_ADMIN --dbpass=$DB_PASSWORD_ADMIN --dbhost=$DB_HOST --dbprefix=mdulcie
 wp core install --allow-root --url=$DB_URL --title=mdulcie.ru --admin_user=$DB_USER_ADMIN  --admin_password=$DB_PASSWORD_ADMIN --admin_email=$ADMIN_MAIL
 wp user create --allow-root $USER_NAME $USER_MAIL --role=author --user_pass=$USER_PASS
+
 # W3TC install and activate plagin for cache (for redis - bonus).
 # echo "define ('FS_METHOD', 'direct');" >> /var/www/html/wordpress/wp-config.php
 # chmod 777 -R /var/www/html/wordpress/wp-content
@@ -18,10 +20,13 @@ wp plugin install redis-cache --allow-root --path='/var/www/html/wordpress'
 
 chmod 666 /var/www/html/wordpress/wp-config.php
 
+sed -i '67d/67r "define( 'WP_DEBUG', false );"' /var/www/html/wordpress/wp-config.php
+
 # https://stackoverflow.com/questions/26009752/replace-line-in-text-file-with-line-from-other-text-file
-sed -i '57d;56r /wp-config_redis.txt' /var/www/html/wordpress/wp-config.php
+sed -i '40,58d;1r /wp-config_redis.txt' /var/www/html/wordpress/wp-config.php
 
 chmod 644 /var/www/html/wordpress/wp-config.php
+fi
 
 wp plugin activate redis-cache --allow-root --path='/var/www/html/wordpress'
 wp redis enable --allow-root --path='/var/www/html/wordpress'
