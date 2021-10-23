80f05a78ce13d09a0e357c37945f01ca7484da62
Maggie Dulcie <mdulcie@si-l2.kzn.21-school.ru>
2021-10-23 19:55:58 +0300

    next step VM

--- inception/srcs/requirements/mariadb/tools/mdb_conf_start.sh
+++ inception/srcs/requirements/mariadb/tools/mdb_conf_start.sh
@@ -2,12 +2,12 @@
 
 # заменяет енв моими из файла
 # sed для поиска и замены строк. echo "The quick brown fox jumps over the lazy dog" | sed 's/dog/cat/g'
-clear_md () {
-  sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<< "$1"
-}
+# clear_md () {
+#   sed -e 's/[&\\/]/\\&/g; s/$/\\/' -e '$s/\\$//' <<< "$1"
+# }
 
-for env_variable in $(grep -o '$[A-Z_]\+' /init.sql | sort | uniq | sed 's/\$//g')
-  do sed -i "s/\$$env_variable/$(clear_md ${!env_variable})/g" /init.sql 
-done
+# for env_variable in $(grep -o '$[A-Z_]\+' /init.sql | sort | uniq | sed 's/\$//g')
+#   do sed -i "s/\$$env_variable/$(clear_md ${!env_variable})/g" /init.sql 
+# done
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql;
-exec "$@"
+/usr/sbin/mysqld --port=3306 --init-file /init.sql
