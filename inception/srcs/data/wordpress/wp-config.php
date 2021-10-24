986e801bb329c2a32c3bfd460b24581fb939b151
Maggie Dulcie <mdulcie@et-n2.kzn.21-school.ru>
2021-10-24 21:08:36 +0300

    for vm

---
+++ inception/srcs/data/wordpress/wp-config.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wp' );
+
+/** MySQL database username */
+define( 'DB_USER', 'mdulcie' );
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'mdulcie' );
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mariadb:3306' );
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+
+/**
+* REDIS 
+* https://wp-kama.ru/plugin/redis-object-cache
+*/
+
+define('WP_REDIS_HOST', 'redis');
+define('WP_REDIS_PORT', '6379');
+define('WP_CACHE_KEY_SALT', 'mdulcie_cache_prefix');
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'mdulcie';
+
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
